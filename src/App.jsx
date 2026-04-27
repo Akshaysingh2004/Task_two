54b9bfa4a297a8d6ae0c5a2b8a376c849f72f9b7
akshay-exiliensoft <akshay.k@exiliensoft.com>
2026-04-28 02:06:01 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import "./App.css";
+import "./index.css";
 
 
 export default function App() {
